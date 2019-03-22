bd64073a5ed5cd01ae454fd92cc2d96f63ea09bf
Stéphane Boucaud <stephane.boucaud@outlook.fr>
2019-03-22 16:14:37 +0100

    Creation d'une RecyclerView et d'un Adapter

--- app/src/main/java/boucaud/stephane/androidfinalproject/MainActivity.java
+++ app/src/main/java/boucaud/stephane/androidfinalproject/MainActivity.java
@@ -2,6 +2,8 @@ package boucaud.stephane.androidfinalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_list);
+
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        MoviesAdapter mAdapter = new MoviesAdapter();
+        recyclerView.setAdapter(mAdapter);
+
     }
 }
